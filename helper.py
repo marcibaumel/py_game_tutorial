838de74df386282aa77399e530f790eeb4d356c7
Marci <marcibaumel@gmail.com>
2024-11-19 22:09:09 +0100

    some updates and linter error fixed

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import pygame
-from sys import exit 
+from sys import exit
 from constants import ASSET_PATH, HEIGHT, WIDTH
 from helper import resource_path
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('GAME TITLE')
+pygame.display.set_caption("GAME TITLE")
+
 clock = pygame.time.Clock()
 
-test_surface = pygame.image.load(resource_path(ASSET_PATH+'Sky.png'))
+font = pygame.font.Font(resource_path(ASSET_PATH + "Pixeltype.ttf"), 50)
+
+sky_surface = pygame.image.load(resource_path(ASSET_PATH + "Sky.png"))
+ground_surface = pygame.image.load(resource_path(ASSET_PATH + "ground.png"))
+text_surface = font.render("Some text", True, "blue")
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(test_surface, (200, 100))
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+
     pygame.display.update()
     clock.tick(60)
-    
