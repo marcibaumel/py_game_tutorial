9d6ee11bce47013cf284ba2a8930827e567b3152
Marci <marcibaumel@gmail.com>
2024-11-20 22:42:50 +0100

    basic rectangle

--- main.py
+++ main.py
@@ -12,9 +12,20 @@ clock = pygame.time.Clock()
 
 font = pygame.font.Font(resource_path(ASSET_PATH + "Pixeltype.ttf"), 50)
 
-sky_surface = pygame.image.load(resource_path(ASSET_PATH + "Sky.png"))
-ground_surface = pygame.image.load(resource_path(ASSET_PATH + "ground.png"))
-text_surface = font.render("Some text", True, "blue")
+sky_surface = pygame.image.load(resource_path(ASSET_PATH + "Sky.png")).convert()
+ground_surface = pygame.image.load(resource_path(ASSET_PATH + "ground.png")).convert()
+
+snail_surface = pygame.image.load(
+    resource_path(ASSET_PATH + "snail1.png")
+).convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom=(600, 300))
+
+text_surface = font.render("GAME TITLE", True, "blue")
+
+player_surf = pygame.image.load(
+    resource_path(ASSET_PATH + "player_walk_1.png")
+).convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -25,6 +36,14 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+
+    snail_rect.x -= 4
+
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)
